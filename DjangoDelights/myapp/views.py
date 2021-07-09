4167344ea7d345637c7cd23eabf0faa6cec26370
TegSingh <tegveer2211@gmail.com>
2021-07-09 00:50:48 -0400

    added template links

--- DjangoDelights/myapp/views.py
+++ DjangoDelights/myapp/views.py
@@ -2,8 +2,22 @@ from django.shortcuts import render
 from .models import Ingredient, RecipeRequirement, MenuItem, Purchase
 from django.views.generic import ListView
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from django.http import Http404
 
 # Create your views here.
+
+# Home view to display the menu when the website opens
+def home(request): 
+    # Check if menu has items
+    try: 
+        menu = MenuItem.objects.all()
+    except Menuitem.DoesNotExist:
+        raise Http404()
+
+    context =  {"menu": menu}
+    # render (request name, template name, object to be passed to template)
+    return render(request, 'myapp/home.html', context)
+
 
 # A view to check all the ingredients in the inventory
 class IngredientsList(ListView):
@@ -20,10 +34,6 @@ class IngredientUpdate(UpdateView):
 # A view to delete ingredients
 class IngredientsDelete(DeleteView):
     Model = Ingredient
-
-# View all items in the menu
-class MenuList(ListView):
-    Model = MenuItem
 
 # View to add Menu Items to the inventory
 class MenuCreate(CreateView):
@@ -46,5 +56,6 @@ class PurchaseCreate(CreateView):
     Model = Purchase
 
 # View the profit and revenue for the restaurant [make this function based]
-def profitAndRevenue(request):
+def finances(request):
     # Calculate Profit and Revenue 
+    pass
